1f197b3b3833234472daaa067bd4d5702c18acc0
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-12-07 01:10:25 +0530

    [Lalit] Add: Inform User if Entered Invalid Mood

--- MoodAnalyzerProblemTest/MoodAnalyzerTest.cs
+++ MoodAnalyzerProblemTest/MoodAnalyzerTest.cs
@@ -19,13 +19,30 @@ namespace MoodAnalyzerProblemTest
             string result = analyzer.AnalyzeMood(); //Act
             Assert.AreEqual(result, "Happy");
         }
-
         [Test]
         public void GivenNullMood_WhenAnalyze_ShouldReturnHappy()
         {
-            MoodAnalyzer analyzer = new MoodAnalyzer(null); //Arrange
-            string result = analyzer.AnalyzeMood(); //Act
-            Assert.AreEqual(result, "Happy");
+            try
+            {
+                MoodAnalyzer analyzer = new MoodAnalyzer(null); //Arrange
+                string result = analyzer.AnalyzeMood(); //Act
+            }
+            catch (MoodAnalyzerException ex)
+            {
+                Assert.AreEqual(ex.Message, "Mood is null");
+            }
+        }
+        public void GivenEmptyMood_WhenAnalyze_ShouldReturnHappy()
+        {
+            try
+            {
+                MoodAnalyzer analyzer = new MoodAnalyzer(string.Empty); //Arrange
+                string result = analyzer.AnalyzeMood(); //Act
+            }
+            catch (MoodAnalyzerException ex)
+            {
+                Assert.AreEqual(ex.Message, "Mood is Empty");
+            }
         }
     }
 }
